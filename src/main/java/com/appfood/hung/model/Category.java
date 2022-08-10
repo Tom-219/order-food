c82ba8a39fd9da24a478e45ebec427751cc60cd4
Tom <kunkka9797@gmail.com>
2022-08-11 00:38:29 +0700

    init

---
+++ src/main/java/com/appfood/hung/model/Category.java
@@ -0,0 +1,47 @@
+package com.appfood.hung.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 50)
+	private String name;
+	
+	@OneToMany(mappedBy = "category")
+	private Set<Product> products = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+}
