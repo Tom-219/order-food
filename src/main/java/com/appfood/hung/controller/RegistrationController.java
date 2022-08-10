5e47b54e3d9cf7470cdeb9521399973ce1d098e4
Tom <kunkka9797@gmail.com>
2022-08-11 00:09:49 +0700

    add register controller

---
+++ src/main/java/com/appfood/hung/controller/RegistrationController.java
@@ -0,0 +1,49 @@
+package com.appfood.hung.controller;
+
+import com.appfood.hung.model.User;
+import com.appfood.hung.payload.UserRegistrationReq;
+import com.appfood.hung.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    @ModelAttribute("user")
+    public UserRegistrationReq userRegistrationDto() {
+        return new UserRegistrationReq();
+    }
+
+    @GetMapping
+    public String showRegistrationForm(Model model) {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationReq userDto,
+                                      BindingResult result) {
+
+        User existing = userService.findByEmail(userDto.getEmail());
+        if (existing != null) {
+            result.rejectValue("email", null, "There is already an account registered with that email");
+        }
+
+        if (result.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(userDto);
+        return "redirect:/registration?success";
+    }
+
+}
