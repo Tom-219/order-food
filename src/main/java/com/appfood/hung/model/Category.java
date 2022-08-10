76ea5cbbb8d1b54d705801d0add29f0376d307f3
Tom <kunkka9797@gmail.com>
2022-08-11 00:42:52 +0700

    init

--- src/main/java/com/appfood/hung/model/Category.java
+++ src/main/java/com/appfood/hung/model/Category.java
@@ -25,6 +25,10 @@ public class Category {
 	@OneToMany(mappedBy = "category")
 	private Set<Product> products = new HashSet<>();
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public Long getId() {
 		return id;
 	}
