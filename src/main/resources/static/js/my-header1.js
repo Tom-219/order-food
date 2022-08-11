146303ada75bc83a1fee9e19676eae2bbcc000c1
Tom <kunkka9797@gmail.com>
2022-08-11 09:29:13 +0700

    no comment

---
+++ src/main/resources/static/js/my-header1.js
@@ -0,0 +1,54 @@
+
+/*class MyHeader extends HTMLElement{
+connectedCallback(){
+
+	this.innerHTML = `
+  <nav class="fh5co-nav" role="navigation">
+		<!-- <div class="top-menu"> -->
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 text-center logo-wrap">
+						<div id="fh5co-logo"><a href="index.html">Tasty<span>.</span></a></div>
+					</div>
+					<div class="col-xs-12 text-center menu-1 menu-wrap">
+						<ul>
+							<li class="active"><a href="/home">Home</a></li>
+							<li><a href="/menu">Menu</a></li>
+							<li class="has-dropdown">
+								<a href="/gallery">Gallery</a>
+								<ul class="dropdown">
+									<li><a href="#">Events</a></li>
+									<li><a href="#">Food</a></li>
+									<li><a href="#">Coffees</a></li>
+								</ul>
+							</li>
+							<li><a href="/reservation">Reservation</a></li>
+							<li><a href="/about">About</a></li>
+							<li><a href="/contact">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		<!-- </div> -->
+	</nav>`
+	}
+}
+
+
+customElements.define('my-header1', MyHeader)*/
+
+class MyHeader extends HTMLElement{
+    connectedCallback(){
+    
+        this.innerHTML = `
+        sadsdas
+        
+        `
+        
+     
+        }
+    }
+    
+    
+    customElements.define('my-header1', MyHeader)
