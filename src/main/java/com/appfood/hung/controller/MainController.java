b328246929fce2df117d126e0bc64ded1c9240ba
Tom <kunkka9797@gmail.com>
2022-08-11 21:03:06 +0700

    git add.

--- src/main/java/com/appfood/hung/controller/MainController.java
+++ src/main/java/com/appfood/hung/controller/MainController.java
@@ -3,6 +3,7 @@ package com.appfood.hung.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
@@ -23,4 +24,34 @@ public class MainController {
     }
     @GetMapping("/index1")
     public String home(){return "index1";}
+
+
+
+    @GetMapping("/about")
+    public String AboutPage() {
+        return "redirect:/about";
+    }
+
+    @GetMapping("/contact")
+    public String Contact() {
+        return "redirect:/contact";
+    }
+
+    @GetMapping("/gallery")
+    public String Gallery() {
+        return "redirect:/gallery";
+    }
+
+    @GetMapping("/menu")
+    public String MenuPage() {
+        return "redirect:/menu";
+    }
+
+    @GetMapping("/reservation")
+    public String ReservationPage() {
+        return "redirect:/reservation";
+    }
+
+
+
 }
