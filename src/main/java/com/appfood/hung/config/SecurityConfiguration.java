09cd54104a0964ac3c8534e1e991f5164e06b6cf
Tom <kunkka9797@gmail.com>
2022-08-13 15:31:05 +0700

    git add.

--- src/main/java/com/appfood/hung/config/SecurityConfiguration.java
+++ src/main/java/com/appfood/hung/config/SecurityConfiguration.java
@@ -46,6 +46,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                     .formLogin()
                         .loginPage("/login")
+                                .defaultSuccessUrl("/listproducts")
                             .permitAll()
                 .and()
                     .logout()
